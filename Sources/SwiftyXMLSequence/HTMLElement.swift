fcd9d48bc1b4dd1f044aaf6459c847c740b22d6a
Sophia Teutschler <sophia@sophiestication.com>
2025-03-14 13:56:02 +0100

    add missing html5 tags

--- Sources/SwiftyXMLSequence/HTMLElement.swift
+++ Sources/SwiftyXMLSequence/HTMLElement.swift
@@ -25,43 +25,73 @@
 import Foundation
 
 public enum HTMLElement: ElementRepresentable {
-    case html,
+    case html, head, meta, title, link, style, script, body
 
-         head, meta, title, link, style, script,
+    // Sectioning
+    case div, span, section, article, aside, nav, header, footer, main
 
-         body, div, span, section, p, br,
-         figure, img, figcaption,
-         a, abbr, cite, q, blockquote, code,
-         sup, sub, strong, b, i, u, small,
-         h1, h2, h3, h4, h5, h6,
-         ul, ol, li, dl, dd,
-         table, tbody, th, tr, td, caption,
+    // Text & Formatting
+    case p, br, hr, figure, figcaption, blockquote, q, cite, code, pre
+    case abbr, sup, sub, strong, b, i, u, small, mark, time, kbd, samp, `var`
+    case ruby, rt, rp, bdi, bdo, wbr
 
-         custom(String)
+    // Headings
+    case h1, h2, h3, h4, h5, h6
+
+    // Lists
+    case ul, ol, li, dl, dt, dd
+
+    // Tables
+    case table, thead, tbody, tfoot, tr, th, td, caption, colgroup, col
+
+    // Forms & Inputs
+    case form, label, input, textarea, button, select, option, optgroup
+    case fieldset, legend, datalist, output, progress, meter
+
+    // Interactive Elements
+    case details, summary, dialog
+
+    // Media
+    case audio, video, source, track, embed, object, param, iframe, canvas, picture, svg, img
+
+    // Links
+    case a
+
+    // Custom
+    case custom(String)
 
     private static let stringToElement: [String: HTMLElement] = [
         "html": .html,
         "head": .head,
-        "title": .title,
         "meta": .meta,
+        "title": .title,
         "link": .link,
-        "script": .script,
         "style": .style,
+        "script": .script,
         "body": .body,
+
         "div": .div,
         "span": .span,
         "section": .section,
-        "figure": .figure,
-        "img": .img,
-        "figcaption": .figcaption,
-        "a": .a,
-        "abbr": .abbr,
-        "cite": .cite,
-        "q": .q,
-        "blockquote": .blockquote,
-        "code": .code,
+        "article": .article,
+        "aside": .aside,
+        "nav": .nav,
+        "header": .header,
+        "footer": .footer,
+        "main": .main,
+
         "p": .p,
         "br": .br,
+        "hr": .hr,
+        "figure": .figure,
+        "figcaption": .figcaption,
+        "blockquote": .blockquote,
+        "q": .q,
+        "cite": .cite,
+        "code": .code,
+        "pre": .pre,
+
+        "abbr": .abbr,
         "sup": .sup,
         "sub": .sub,
         "strong": .strong,
@@ -69,23 +99,76 @@ public enum HTMLElement: ElementRepresentable {
         "i": .i,
         "u": .u,
         "small": .small,
+        "mark": .mark,
+        "time": .time,
+        "kbd": .kbd,
+        "samp": .samp,
+        "var": .var,
+        "ruby": .ruby,
+        "rt": .rt,
+        "rp": .rp,
+        "bdi": .bdi,
+        "bdo": .bdo,
+        "wbr": .wbr,
+
         "h1": .h1,
         "h2": .h2,
         "h3": .h3,
         "h4": .h4,
         "h5": .h5,
         "h6": .h6,
+
         "ul": .ul,
         "ol": .ol,
         "li": .li,
         "dl": .dl,
+        "dt": .dt,
         "dd": .dd,
+
         "table": .table,
+        "thead": .thead,
         "tbody": .tbody,
+        "tfoot": .tfoot,
         "tr": .tr,
-        "td": .td,
         "th": .th,
-        "caption": .caption
+        "td": .td,
+        "caption": .caption,
+        "colgroup": .colgroup,
+        "col": .col,
+
+        "form": .form,
+        "label": .label,
+        "input": .input,
+        "textarea": .textarea,
+        "button": .button,
+        "select": .select,
+        "option": .option,
+        "optgroup": .optgroup,
+        "fieldset": .fieldset,
+        "legend": .legend,
+        "datalist": .datalist,
+        "output": .output,
+        "progress": .progress,
+        "meter": .meter,
+
+        "details": .details,
+        "summary": .summary,
+        "dialog": .dialog,
+
+        "audio": .audio,
+        "video": .video,
+        "source": .source,
+        "track": .track,
+        "embed": .embed,
+        "object": .object,
+        "param": .param,
+        "iframe": .iframe,
+        "canvas": .canvas,
+        "picture": .picture,
+        "svg": .svg,
+        "img": .img,
+
+        "a": .a,
     ]
 
     public init(element: String, attributes: Attributes) {
